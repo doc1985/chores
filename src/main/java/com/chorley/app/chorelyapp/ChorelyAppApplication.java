5f5d6a270a2bd582cdfdb219d6d0b0e39e62cc32
Jim Belcher <jbelcher1985@yahoo.com>
2022-07-26 22:25:50 -0400

    initial commit

---
+++ src/main/java/com/chorley/app/chorelyapp/ChorelyAppApplication.java
@@ -0,0 +1,13 @@
+package com.chorley.app.chorelyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChorelyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChorelyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chorley/app/chorelyapp/ChorelyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chorley.app.chorelyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChorelyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
